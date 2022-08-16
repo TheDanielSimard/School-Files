37cde6b49a0588ab5cdaaadeaf13bd3e796d4803
TheDanielSimard <111359959+TheDanielSimard@users.noreply.github.com>
2022-08-16 10:01:08 -0400

    Add files via upload

---
+++ Assignment 7 BreakEven/Assignment7BreakEven/BreakEven.cs
@@ -0,0 +1,104 @@
+﻿//Daniel Simard
+//October 20th, 2021
+//Assignment 7 - BreakEven
+//Calculate Companies Sales and Costs
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment7BreakEven
+{
+    class BreakEven
+    {
+        //Variables
+        string companyName;
+        double sellingPrice;
+        double overHead;
+        double administrativeSalaries;
+        double materialCostPercent;
+        double laborCostPercent;
+        double variablePercent;
+        double contributionMargin;
+        double fixedCosts;
+        double breakEvenUnits;
+        double breakEvenSales;
+        
+        //Properties
+        public string CompanyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+        public double SellingPrice
+        {
+            get { return sellingPrice; }
+            set { sellingPrice = value; }
+        }
+        public double OverHead
+        {
+            get { return overHead; }
+            set { overHead = value; }
+        }
+        public double AdministrativeSalaries
+        {
+            get { return administrativeSalaries; }
+            set { administrativeSalaries = value; }
+        }
+        public double MaterialCostPercent
+        {
+            get { return materialCostPercent; }
+            set { materialCostPercent = value; }
+        }
+        public double LaborCostPercent
+        {
+            get { return laborCostPercent; }
+            set { laborCostPercent = value; }
+        }
+        public double VariablePercent
+        {
+            get { return variablePercent; }
+            set { variablePercent = value; }
+        }
+        public double ContributionMargin
+        {
+            get { return contributionMargin; }
+            set { contributionMargin = value; }
+        }
+        public double FixedCosts
+        {
+            get { return fixedCosts; }
+            set { fixedCosts = value; }
+        }
+        public double BreakEvenUnits
+        {
+            get { return breakEvenUnits; }
+            set { breakEvenUnits = value; }
+        }
+        public double BreakEvenSales
+        {
+            get { return breakEvenSales; }
+            set { breakEvenSales = value; }
+        }
+        //Calculations
+        private void BreakEvenCalculations()
+        {
+            FixedCosts = OverHead + AdministrativeSalaries;
+            VariablePercent = MaterialCostPercent + LaborCostPercent;
+            ContributionMargin = SellingPrice * VariablePercent;
+            BreakEvenUnits = Math.Ceiling(FixedCosts / ContributionMargin * 100); //Converting to percentage
+            BreakEvenSales = BreakEvenUnits * SellingPrice;
+        }
+        //Sends to main
+        public override string ToString()
+        {
+            //Using interpolation operator "$" to embed a value(s) within the string
+            BreakEvenCalculations();
+            return string.Format("*******************************************************************" +
+                $"\n\t{CompanyName}" +
+                $"\nTotal fixed costs\t{FixedCosts:C}" +
+                $"\nTotal variable percent\t{VariablePercent:F2}%" +
+                $"\nBreak even units are\t{BreakEvenUnits}" +
+                $"\nBreak even sales are\t{BreakEvenSales:C}");
+        }
+    }
+}
---
+++ Assignment 7 BreakEven/Assignment7BreakEven/Program.cs
@@ -0,0 +1,54 @@
+﻿//Daniel Simard
+//October 20th, 2021
+//Assignment 7 - BreakEven
+//Calculate Companies Sales and Costs
+using System;
+using static System.Console;
+namespace Assignment7BreakEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Header_and_Instructions();
+            WriteLine("*******************************************************************");
+            BreakEven calculations = new BreakEven();
+
+            Write("Enter company name: ");
+            calculations.CompanyName = ReadLine();
+
+            Write("Enter selling price of product: ");
+            calculations.SellingPrice = double.Parse(ReadLine());
+
+            Write("Enter fixed overhead cost: ");
+            calculations.OverHead = double.Parse(ReadLine());
+
+            Write("Enter fixed administrative salaries: ");
+            calculations.AdministrativeSalaries = double.Parse(ReadLine());
+
+            Write("Enter raw material percent of selling price: ");
+            calculations.MaterialCostPercent = double.Parse(ReadLine());
+
+            Write("Enter labor cost percent of selling price: ");
+            calculations.LaborCostPercent = double.Parse(ReadLine());
+
+            WriteLine(calculations);      
+            Read();
+        }
+        static void Header_and_Instructions()
+        {
+        string date = DateTime.Today.ToShortDateString();
+            WriteLine("*******************************************************************\n\t" +
+                "BreakEven App\n\t" +
+                "Calculates breakeven in units and dollars for businesses\n\t" +
+                "that are applying for financing.\n\t" +
+                "Daniel Simard\n\t" +
+               $"{date}");
+            WriteLine("*******************************************************************\n" +
+                "This program will determine if your company can recieve financing.\n" +
+                "You will be asked to enter selling price of a product, fixed\n" +
+                "overhead costs, fixed administrative salaries, raw materials\n" +
+                "cost percentage, and labor costs percentage.");
+        }
+    }
+}
