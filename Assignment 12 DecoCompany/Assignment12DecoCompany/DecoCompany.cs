b2cc94cd0fd4518d1aeb3150cef843e2ebf81665
TheDanielSimard <111359959+TheDanielSimard@users.noreply.github.com>
2022-08-16 09:59:06 -0400

    Add files via upload

---
+++ Assignment 12 DecoCompany/Assignment12DecoCompany/DecoCompany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+namespace Assignment12DecoCompany
+{
+    class DecoCompany
+    {
+        string[] companyNames;
+        double[] amountOwed;
+        string companyName;
+        public string[] CompanyNames
+        {
+            set { companyNames = value; }
+        }
+        public double[] AmountedOwed
+        {
+            set { amountOwed = value; }
+        }
+        public string CompanyName
+        {
+            set { companyName = value; }
+        }
+        public void FindCompany()
+        {
+            for (int i = 0; i < companyNames.Length; i++)
+            {
+                if (companyNames[i] == companyName.ToUpper())
+                {
+                    WriteLine("\n******************************************************");
+                    WriteLine($"{companyNames[i]}\nPayable Amount is {amountOwed[i]:C}");
+                    return;
+                }
+            }
+            WriteLine("\n******************************************************");
+            WriteLine("Company not found");
+        }
+        public override string ToString()
+        {
+            string str = string.Empty;
+
+            WriteLine("Name\tPayable");
+            for (int i = 0; i < companyNames.Length; i++)
+            {
+                WriteLine($"{companyNames[i]}\t{amountOwed[i]:C}");
+            }
+            WriteLine("******************************************************");
+            return str;
+        }
+    }
+}
---
+++ Assignment 12 DecoCompany/Assignment12DecoCompany/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+namespace Assignment12DecoCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] companyNames = { "OXONY", "CORE", "ITECH", "APPLE" };
+            double[] amountOwed = { 25000, 37000, 10000, -500 };
+
+            Header_Instructions();
+
+            DecoCompany Company = new DecoCompany();
+            Company.CompanyNames = companyNames;
+            Company.AmountedOwed = amountOwed;
+
+            Write(Company);
+
+            Write("Enter name of company: ");
+            Company.CompanyName = ReadLine();
+
+            Company.FindCompany();
+
+            Read();
+        }
+        private static void Header_Instructions()
+        {
+            string date = DateTime.Today.ToShortDateString();
+            WriteLine("******************************************************");
+            WriteLine("\tAccounts Payable");
+            WriteLine("\tList accounts payable with total money owed,");
+            WriteLine("\tand determine money owed by a specific company");
+            WriteLine("\tDaniel Simard");
+            WriteLine($"\t{date}");
+            WriteLine("******************************************************");
+            WriteLine("This program will display a list of accounts payable");
+            WriteLine("and total money owed.");
+            WriteLine("It will also find money owed for a specific account.");
+            WriteLine("When prompted enter company's name to determine money");
+            WriteLine("owed by that company.");
+            WriteLine("******************************************************");
+        }
+    }
+}
