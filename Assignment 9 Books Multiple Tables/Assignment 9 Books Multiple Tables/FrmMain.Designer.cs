5b7395b0ec8ef9067e317394be3d6aa2a9788f34
TheDanielSimard <111359959+TheDanielSimard@users.noreply.github.com>
2022-08-16 09:56:20 -0400

    Add files via upload

---
+++ Assignment 9 Books Multiple Tables/Assignment 9 Books Multiple Tables/FrmMain.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace Assignment_9_Books_Multiple_Tables
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label customerIDLabel;
+            System.Windows.Forms.Label nameLabel;
+            System.Windows.Forms.Label addressLabel;
+            System.Windows.Forms.Label cityLabel;
+            System.Windows.Forms.Label stateLabel;
+            System.Windows.Forms.Label zipCodeLabel;
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cboName = new System.Windows.Forms.ComboBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.invoiceDataGridView = new System.Windows.Forms.DataGridView();
+            this.invoicesDataGridView = new Assignment_9_Books_Multiple_Tables.MMABooksDataSet();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            customerIDLabel = new System.Windows.Forms.Label();
+            nameLabel = new System.Windows.Forms.Label();
+            addressLabel = new System.Windows.Forms.Label();
+            cityLabel = new System.Windows.Forms.Label();
+            stateLabel = new System.Windows.Forms.Label();
+            zipCodeLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.invoiceDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invoicesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerIDLabel
+            // 
+            customerIDLabel.AutoSize = true;
+            customerIDLabel.Location = new System.Drawing.Point(259, 94);
+            customerIDLabel.Name = "customerIDLabel";
+            customerIDLabel.Size = new System.Drawing.Size(89, 17);
+            customerIDLabel.TabIndex = 13;
+            customerIDLabel.Text = "Customer ID:";
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new System.Drawing.Point(259, 63);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new System.Drawing.Size(49, 17);
+            nameLabel.TabIndex = 15;
+            nameLabel.Text = "Name:";
+            // 
+            // addressLabel
+            // 
+            addressLabel.AutoSize = true;
+            addressLabel.Location = new System.Drawing.Point(259, 123);
+            addressLabel.Name = "addressLabel";
+            addressLabel.Size = new System.Drawing.Size(64, 17);
+            addressLabel.TabIndex = 17;
+            addressLabel.Text = "Address:";
+            // 
+            // cityLabel
+            // 
+            cityLabel.AutoSize = true;
+            cityLabel.Location = new System.Drawing.Point(259, 152);
+            cityLabel.Name = "cityLabel";
+            cityLabel.Size = new System.Drawing.Size(35, 17);
+            cityLabel.TabIndex = 19;
+            cityLabel.Text = "City:";
+            // 
+            // stateLabel
+            // 
+            stateLabel.AutoSize = true;
+            stateLabel.Location = new System.Drawing.Point(259, 181);
+            stateLabel.Name = "stateLabel";
+            stateLabel.Size = new System.Drawing.Size(45, 17);
+            stateLabel.TabIndex = 21;
+            stateLabel.Text = "State:";
+            // 
+            // zipCodeLabel
+            // 
+            zipCodeLabel.AutoSize = true;
+            zipCodeLabel.Location = new System.Drawing.Point(259, 210);
+            zipCodeLabel.Name = "zipCodeLabel";
+            zipCodeLabel.Size = new System.Drawing.Size(69, 17);
+            zipCodeLabel.TabIndex = 23;
+            zipCodeLabel.Text = "Zip Code:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(768, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(354, 91);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(121, 23);
+            this.txtID.TabIndex = 14;
+            // 
+            // cboName
+            // 
+            this.cboName.FormattingEnabled = true;
+            this.cboName.Location = new System.Drawing.Point(354, 60);
+            this.cboName.Name = "cboName";
+            this.cboName.Size = new System.Drawing.Size(121, 25);
+            this.cboName.TabIndex = 16;
+            this.cboName.SelectionChangeCommitted += new System.EventHandler(this.cboName_SelectionChangeCommitted);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(354, 120);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(121, 23);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(354, 149);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(121, 23);
+            this.txtCity.TabIndex = 20;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(354, 178);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(121, 23);
+            this.txtState.TabIndex = 22;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(354, 207);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(121, 23);
+            this.txtZip.TabIndex = 24;
+            // 
+            // invoiceDataGridView
+            // 
+            this.invoiceDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.invoiceDataGridView.Location = new System.Drawing.Point(11, 236);
+            this.invoiceDataGridView.Name = "invoiceDataGridView";
+            this.invoiceDataGridView.Size = new System.Drawing.Size(745, 334);
+            this.invoiceDataGridView.TabIndex = 24;
+            // 
+            // invoicesDataGridView
+            // 
+            this.invoicesDataGridView.DataSetName = "MMABooksDataSet";
+            this.invoicesDataGridView.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 582);
+            this.Controls.Add(this.invoiceDataGridView);
+            this.Controls.Add(customerIDLabel);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(nameLabel);
+            this.Controls.Add(this.cboName);
+            this.Controls.Add(addressLabel);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(cityLabel);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(stateLabel);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(zipCodeLabel);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmMain";
+            this.Text = "Customer Invoices";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.invoiceDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invoicesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cboName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.DataGridView invoiceDataGridView;
+        private MMABooksDataSet invoicesDataGridView;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Assignment 9 Books Multiple Tables/Assignment 9 Books Multiple Tables/MMADataClass.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment_9_Books_Multiple_Tables
+{
+    class MMADataClass
+    {
+        MMABooksDataSetTableAdapters.InvoicesTableAdapter invoicesTableAdapter;
+        MMABooksDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        MMABooksDataSet mmaDataSet;
+
+        public MMABooksDataSet GetDataSet()
+        {
+            invoicesTableAdapter = new MMABooksDataSetTableAdapters.InvoicesTableAdapter();
+            customersTableAdapter = new MMABooksDataSetTableAdapters.CustomersTableAdapter();
+            mmaDataSet = new MMABooksDataSet();
+
+            invoicesTableAdapter.Fill(mmaDataSet.Invoices);
+            customersTableAdapter.Fill(mmaDataSet.Customers);
+            return mmaDataSet;
+        }
+    }
+}
