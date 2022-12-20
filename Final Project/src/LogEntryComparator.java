bf1c6af00ef1f7780ca6417bf2bdc169db322c04
TheDanielSimard <111359959+TheDanielSimard@users.noreply.github.com>
2022-12-20 09:57:17 -0500

    Delete Final Project directory

--- Final Project/src/LogEntryComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class LogEntryComparator implements Comparator<LogEntry> {
-    // override the compare() method
-    public int compare(LogEntry s1, LogEntry s2) {
-        return s1.getTime().compareTo(s2.getTime());
-    }
-}
